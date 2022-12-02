bd7863ce1dcddc58e3594bed29d1b19002266240
joyfultiger555 <114970320+joyfultiger555@users.noreply.github.com>
2022-12-02 15:16:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import politicianInfo from "./assets/politician-info.json";
+import {PoliticianItem} from "./components/PoliticianItem.js"
+import { useEffect, useState } from 'react';
+import { Checkbox, Collapse } from 'antd';
+import { propTypes } from "react-bootstrap/esm/Image";
+import CheckBox from '/Users/noahatanda/Desktop/uiux/Development-Anonymous/src/components/CheckBox.js'
+
+import Display from '/Users/noahatanda/Desktop/uiux/Development-Anonymous/src/components/Display.js'
+function App() {
+  return (
+    <Display/>
+  );
+}
+
+export default App;
